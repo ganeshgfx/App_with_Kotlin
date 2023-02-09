d0f062e650e975b605ff44af07cf3951639225f2
GaneSh GFX <43039255+ganeshgfx@users.noreply.github.com>
2023-02-09 12:09:52 +0530

    async wait

--- app/src/main/java/com/example/androidwithkotlin/MainActivity.kt
+++ app/src/main/java/com/example/androidwithkotlin/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import com.example.androidwithkotlin.ViewModel.MainActivityViewModelFactory
 import com.example.viewmodelwithkotlin.MainActivityViewModel
@@ -15,6 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var button: ExtendedFloatingActionButton
     lateinit var text: TextView
+    lateinit var textSpeed: TextView
 
     val TAG = "TAG"
 
@@ -28,31 +30,35 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
 
-        mainViewModel = ViewModelProvider(this, MainActivityViewModelFactory(0)).get(MainActivityViewModel::class.java)
+        mainViewModel = ViewModelProvider(
+            this,
+            MainActivityViewModelFactory(0)
+        ).get(MainActivityViewModel::class.java)
+
+        text = findViewById(R.id.text)
+        textSpeed = findViewById(R.id.size)
 
         button = findViewById(R.id.incrementBTN)
         button.setOnClickListener {
 
 
-
-            MainScope().launch(Dispatchers.Default){
+            MainScope().launch(Dispatchers.Default) {
                 // Log.d(TAG, "CoroutineScope - ${Thread.currentThread().name}")
-
             }
 
             //Coroutines
-            CoroutineScope(Dispatchers.IO).launch{
-               // Log.d(TAG, "CoroutineScope - ${Thread.currentThread().name}")
+            CoroutineScope(Dispatchers.IO).launch {
+                // Log.d(TAG, "CoroutineScope - ${Thread.currentThread().name}")
             }
             GlobalScope.launch(Dispatchers.Main) {
                 // Log.d(TAG, "GlobalScope - ${Thread.currentThread().name}")
-               loading()
+                async { loading() }
+
             }
             MainScope().launch(Dispatchers.Default) {
-               // Log.d(TAG, "MainScope - ${Thread.currentThread().name}")
+                // Log.d(TAG, "MainScope - ${Thread.currentThread().name}")
 
             }
-
             CoroutineScope(Dispatchers.Main).launch {
                 task1()
             }
@@ -60,38 +66,82 @@ class MainActivity : AppCompatActivity() {
                 task2()
             }
 
+            //jobs
+            CoroutineScope(Dispatchers.IO).launch {
+                printFileSize2()
+            }
+            //async wait
+            CoroutineScope(Dispatchers.Main).launch{
+                var size = async{ getFileSize() }
+                var time = async { getTime() }
+                textSpeed.setText("${size.await()}GB - ${time.await()}s")
+            }
+
         }
-        button.setOnLongClickListener{
+        button.setOnLongClickListener {
             mainViewModel.decrement()
             setCount()
             true
         }
-        text = findViewById(R.id.text)
         setCount()
     }
-    fun setCount(){
-            text.text = "Downloading... ${mainViewModel.count.toString()}"
+
+    //coroutine j0b
+    private suspend fun printFileSize() {
+        var fileSize = 0
+        var job = CoroutineScope(Dispatchers.IO).launch {
+            fileSize = getFileSize()
+        }
+        job.join()
+        //Toast.makeText(this, "File size is $fileSize GB", Toast.LENGTH_SHORT).show()
+        Log.d(TAG, "printFileSize: $fileSize GB")
+    }
+    //async
+    private suspend fun printFileSize2() {
+        var job = CoroutineScope(Dispatchers.IO).async {
+            getFileSize()
+        }
+        //Toast.makeText(this, "File size is $fileSize GB", Toast.LENGTH_SHORT).show()
+        Log.d(TAG, "printFileSize: ${job.await()} GB")
+    }
+
+    fun setCount() {
+        text.text = "Downloading... ${mainViewModel.count.toString()}"
     }
 
     //Coroutine Suspension
-    suspend fun task1(){
-        Log.d(TAG, "Task1: Started")
+    suspend fun task1() {
+        // Log.d(TAG, "Task1: Started")
         yield()
-        Log.d(TAG, "Task1: Stopped")
+        //Log.d(TAG, "Task1: Stopped")
     }
-    suspend fun task2(){
-        Log.d(TAG, "task2: Started")
+
+    suspend fun task2() {
+        //Log.d(TAG, "task2: Started")
         yield()
-        Log.d(TAG, "task2: Stopped")
+        //Log.d(TAG, "task2: Stopped")
     }
-    suspend fun loading(){
+
+    //suspended
+    suspend fun loading() {
         mainViewModel.reset()
-        for (i in 1..100){
+        for (i in 1..100) {
             val timeMillis = (10..50).random().toLong()
             //Log.d(TAG, timeMillis.toString())
             delay(timeMillis)
             mainViewModel.increaseCount()
             setCount()
         }
+    }
+
+    suspend fun getFileSize(): Int {
+        delay(5000)
+        return 19
+    }
+
+    suspend fun getTime():Int{
+        val timeMillis = (10..500).random().toLong()
+        delay(timeMillis)
+        return (1..5).random()
     }
 }
