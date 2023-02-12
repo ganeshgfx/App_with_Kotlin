a90f99b7bc8891d0fd40bc2f6561d7f67ea3a38d
GaneSh GFX <43039255+ganeshgfx@users.noreply.github.com>
2023-02-12 16:17:58 +0530

    viewBinding added

--- app/src/main/java/com/example/androidwithkotlin/Home.kt
+++ app/src/main/java/com/example/androidwithkotlin/Home.kt
@@ -7,11 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.example.androidwithkotlin.databinding.FragmentHomeBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+
 
 /**
  * A simple [Fragment] subclass.
@@ -22,6 +24,9 @@ class Home : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    private var _bind : FragmentHomeBinding? = null
+    private val binding get() = _bind!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,8 +42,10 @@ class Home : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val inflate = inflater.inflate(R.layout.fragment_home, container, false)
-        val button = inflate.findViewById<Button>(R.id.logout)
-        button.setOnClickListener {
+
+        _bind = FragmentHomeBinding.inflate(inflater,container,false)
+
+        binding.logout.setOnClickListener {
             findNavController().navigate(R.id.action_home2_to_login)
         }
         return inflate
